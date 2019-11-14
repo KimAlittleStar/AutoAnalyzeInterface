fcd306d9000843ec4cd10d8868fb33bd433f04f3
KimAlittleStar <kim_alittlestar@qq.com>
2019-11-14 23:29:47 +0800

    更新mystring中的splits函数

--- mystring.cpp
+++ mystring.cpp
@@ -2,26 +2,24 @@
 #include <string>
 MyString::MyString()
 {
-
 }
-MyString::MyString(const MyString& other)
+MyString::MyString(const MyString &other)
 {
     std::string::append(other);
 }
-MyString::MyString(const char* str)
+MyString::MyString(const char *str)
 {
     std::string::append(str);
 }
-MyString::MyString(const unsigned char* str)
+MyString::MyString(const unsigned char *str)
 {
-    std::string::append(reinterpret_cast<const char*> (str));
+    std::string::append(reinterpret_cast<const char *>(str));
 }
 
-MyString::MyString(const std::string & other)
+MyString::MyString(const std::string &other)
 {
     std::string::append(other);
 }
-
 
 /**
  ***********************************************************************************************
@@ -37,25 +35,23 @@ MyString::MyString(const std::string & other)
  * @note mark your ideal
  **********************************************************************************************
  */
-int MyString::replace(const std::string & des ,const std::string & src,int index)
+int MyString::replace(const std::string &des, const std::string &src, int index)
 {
     std::string::size_type pos = 0;
     int cnt = 1;
     int ret = -1;
-    while((pos = this->find(des,pos+(!!pos)*(src.size()-1))) != std::string::npos)   //替换所有指定子串
+    while ((pos = this->find(des, pos + (!!pos) * (src.size() - 1))) != std::string::npos) //替换所有指定子串
     {
-       // replace();
-        if(ret == -1)
+        // replace();
+        if (ret == -1)
             ret = 0;
-        if(cnt == index || index == 0)
+        if (cnt == index || index == 0)
         {
             std::string::replace(pos, des.length(), src);
             ret++;
             pos++;
-
         }
         cnt++;
-
     }
     return ret;
 }
@@ -73,9 +69,9 @@ int MyString::replace(const std::string & des ,const std::string & src,int index
  * @note mark your ideal
  **********************************************************************************************
  */
-int MyString::replace(const char * des,const char * src,int index)
+int MyString::replace(const char *des, const char *src, int index)
 {
-    return replace(std::string(des),std::string(src),index);
+    return replace(std::string(des), std::string(src), index);
 }
 
 /**
@@ -91,9 +87,9 @@ int MyString::replace(const char * des,const char * src,int index)
  * @note mark your ideal
  **********************************************************************************************
  */
-int MyString::replace(const std::string & des ,const char* src,int index)
+int MyString::replace(const std::string &des, const char *src, int index)
 {
-    return replace(des,std::string(src),index);
+    return replace(des, std::string(src), index);
 }
 
 /**
@@ -109,9 +105,9 @@ int MyString::replace(const std::string & des ,const char* src,int index)
  * @note mark your ideal
  **********************************************************************************************
  */
-int MyString::replace(const char* des ,const std::string & src,int index)
+int MyString::replace(const char *des, const std::string &src, int index)
 {
-    return replace(std::string(des),src,index);
+    return replace(std::string(des), src, index);
 }
 
 /**
@@ -125,23 +121,22 @@ int MyString::replace(const char* des ,const std::string & src,int index)
  * @note mark your ideal
  **********************************************************************************************
  */
-MyString MyString::mid(int start,int lengh) const
+MyString MyString::mid(int start, int lengh) const
 {
-    return mid(static_cast<size_t>( start),static_cast<size_t>(lengh));
+    return mid(static_cast<size_t>(start), static_cast<size_t>(lengh));
 }
-MyString MyString::mid(std::size_t start,std::size_t lengh) const
+MyString MyString::mid(std::size_t start, std::size_t lengh) const
 {
-    return MyString(this->substr(start,lengh));
+    return MyString(this->substr(start, lengh));
 }
-MyString MyString::mid(int start,std::size_t lengh) const
+MyString MyString::mid(int start, std::size_t lengh) const
 {
-    return mid(static_cast<size_t>( start),lengh);
+    return mid(static_cast<size_t>(start), lengh);
 }
-MyString MyString::mid(std::size_t start,int lengh) const
+MyString MyString::mid(std::size_t start, int lengh) const
 {
-    return mid(start,static_cast<size_t>(lengh));
+    return mid(start, static_cast<size_t>(lengh));
 }
-
 
 std::vector<MyString> MyString::splits(const MyString &ftm) const
 {
@@ -149,76 +144,83 @@ std::vector<MyString> MyString::splits(const MyString &ftm) const
     /// @bug 如果第一个字符就是匹配字符,那么就会降低一个字符也包括进去
     /// @todo 把lastindex 初始化为-1 所有的事情都可以迎刃而解;
     int smartmode = 0;
-    if((ftm[0] == '(' && ftm[ftm.size()-1] == ')') ||
-       (ftm[0] == '[' && ftm[ftm.size()-1] == ']') ||
-       (ftm[0] == '{' && ftm[ftm.size()-1] == '}') ||
-       (ftm[0] == '<' && ftm[ftm.size()-1] == '>') ||
-       (ftm[0] == '\"' && ftm[ftm.size()-1] == '\"') ||
-       (ftm[0] == '\'' && ftm[ftm.size()-1] == '\'') )
-        smartmode = 1;
+    char lastflagchar = '\0';
+    while (isCouple(ftm[smartmode], ftm[ftm.size() - 1 - smartmode]) && smartmode < static_cast<int>((ftm.size() / 2)))
+        smartmode++;
     std::vector<MyString> ret;
     ret.clear();
-    size_t lastindex = std::string::npos ;
+    size_t lastindex = std::string::npos;
     size_t flag = 0;
     MyString temp;
     int deep = 0;
-    for(size_t i = 0;i<size();i++)
+    for (size_t i = 0; i < size(); i++)
     {
-        if((flag = ftm.find(at(i))) != std::string::npos)
+        if ((flag = ftm.find(at(i))) != std::string::npos)
         {
-            if(smartmode == 0)
-            {//没有配对出现的标志
-                if(((i-lastindex) <2 ) || i== 0)
-                {//检测到两个相邻的标致
+            if (smartmode == 0)
+            { //没有配对出现的标志
+                if (((i - lastindex) < 2) || i == 0)
+                { //检测到两个相邻的标致
                     lastindex = i;
-                }else
+                }
+                else
                 {
-                    temp = substr(lastindex+1,i-lastindex-1);
+                    temp = substr(lastindex + 1, i - lastindex - 1);
                     ret.push_back(temp);
                     lastindex = i;
                 }
-            }else
-            {//出现需要配对的情况
-                if(flag == 0)
+            }
+            else
+            { //出现需要配对的情况
+                if (ftm[flag] == lastflagchar && ftm[flag] == ftm[ftm.size() - flag - 1] && deep == 1)
                 {
-                    if(deep == 0)           //如果现在检测到成对的第一个字符且深度为零,清空前面的字符作为一个整体
+                    flag = ftm.size() - flag - 1;
+                }
+                if (static_cast<int>(flag) < smartmode)
+                {
+                    if (deep == 0) //如果现在检测到成对的第一个字符且深度为零,清空前面的字符作为一个整体
                     {
-                        if(((i-lastindex) <2) || i== 0)
-                        {//检测到两个相邻的标致
+                        lastflagchar = ftm[flag];
+                        if (((i - lastindex) < 2) || i == 0)
+                        { //检测到两个相邻的标致
                             lastindex = i;
-                        }else
+                        }
+                        else
                         {
-                            temp = substr(lastindex+1,i-lastindex-1);
+                            temp = substr(lastindex + 1, i - lastindex - 1);
                             ret.push_back(temp);
                             lastindex = i;
                         }
                     }
-                    deep++;
-
-                }else if(flag == (ftm.size()-1))
-                {//如果是配对的字符且深度为1 那么整个时候才能把字符串截取,否则中间全部跳过
+                    if (ftm[flag] == lastflagchar)
+                        deep++;
+                }
+                else if (isCouple(ftm[flag], lastflagchar))
+                { //如果是配对的字符且深度为1 那么整个时候才能把字符串截取,否则中间全部跳过
                     deep--;
-                    if(deep == 0)
+                    if (deep == 0)
                     {
-                        if(((i-lastindex) <2) || i== 0)
-                        {//检测到两个相邻的标致
+                        if (((i - lastindex) < 2) || i == 0)
+                        { //检测到两个相邻的标致
                             lastindex = i;
-                        }else
+                        }
+                        else
                         {
-                            temp = substr(lastindex+1,i-lastindex-1);
+                            temp = substr(lastindex + 1, i - lastindex - 1);
                             ret.push_back(temp);
                             lastindex = i;
                         }
                     }
-
-                }else if(deep == 0)
+                }
+                else if (deep == 0)
                 {
-                    if(((i-lastindex) <2) || i== 0)
-                    {//检测到两个相邻的标致
+                    if (((i - lastindex) < 2) || i == 0)
+                    { //检测到两个相邻的标致
                         lastindex = i;
-                    }else
+                    }
+                    else
                     {
-                        temp = substr(lastindex+1,i-lastindex-1);
+                        temp = substr(lastindex + 1, i - lastindex - 1);
                         ret.push_back(temp);
                         lastindex = i;
                     }
@@ -226,27 +228,39 @@ std::vector<MyString> MyString::splits(const MyString &ftm) const
             }
         }
     }
-    if((size()-lastindex) > 1)
+    if ((size() - lastindex) > 1)
     {
-        temp = substr(lastindex+1,size()-lastindex-1);
+        temp = substr(lastindex + 1, size() - lastindex - 1);
         ret.push_back(temp);
     }
     return ret;
 }
 
-
-
-
-
-
-
-void MyString::removeAll(const char * str)
+void MyString::removeAll(const char *str)
 {
-    replace(str,"");
+    replace(str, "");
 }
 
-
-void  MyString::removeAll(const MyString & str)
+void MyString::removeAll(const MyString &str)
 {
-    replace(str,"");
+    replace(str, "");
 }
+
+bool MyString::isCouple(const char a, const char b) const
+{
+    return ((a == '\'' && b == '\'') ||
+            (a == '\"' && b == '\"') ||
+            (a == '(' && b == ')') ||
+            (a == ')' && b == '(') ||
+            (a == '[' && b == ']') ||
+            (a == ']' && b == '[') ||
+            (a == '{' && b == '}') ||
+            (a == '}' && b == '{') ||
+            (a == '<' && b == '>') ||
+            (a == '>' && b == '<'));
+}
+
+// bool MyString::isCouple(int a, int b)
+// {
+//     return true;
+// }
--- mystring.h
+++ mystring.h
@@ -1,31 +1,32 @@
 #ifndef MYSTRING_H
 #define MYSTRING_H
-#include<string>
+#include <string>
 #include <vector>
-class MyString :public std::string
+class MyString : public std::string
 {
 public:
     MyString();
-    MyString(const MyString& other);
-    MyString(const char* str);
-    MyString(const unsigned char* str);
-    MyString(const std::string & other);
+    MyString(const MyString &other);
+    MyString(const char *str);
+    MyString(const unsigned char *str);
+    MyString(const std::string &other);
 
-    int replace(const char * des,const char * src,int index = 0);
-    int replace(const std::string & des ,const std::string & src,int index = 0);
-    int replace(const std::string & des ,const char* src,int index = 0);
-    int replace(const char* des ,const std::string & src,int index = 0);
+    int replace(const char *des, const char *src, int index = 0);
+    int replace(const std::string &des, const std::string &src, int index = 0);
+    int replace(const std::string &des, const char *src, int index = 0);
+    int replace(const char *des, const std::string &src, int index = 0);
 
-    MyString mid(int start,int lengh)   const;
-    MyString mid(std::size_t start,std::size_t lengh) const;
-    MyString mid(int start,std::size_t lengh) const;
-    MyString mid(std::size_t start,int lengh) const;
+    MyString mid(int start, int lengh) const;
+    MyString mid(std::size_t start, std::size_t lengh) const;
+    MyString mid(int start, std::size_t lengh) const;
+    MyString mid(std::size_t start, int lengh) const;
 
-    std::vector<MyString> splits(const MyString & ftm) const;
+    std::vector<MyString> splits(const MyString &ftm) const;
 
-    void removeAll(const char * str);
-    void removeAll(const MyString & str);
+    void removeAll(const char *str);
+    void removeAll(const MyString &str);
 
+    bool isCouple(const char a, const char b) const;
 };
 
 #endif // MYSTRING_H
